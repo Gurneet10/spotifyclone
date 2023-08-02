2a8e233cef3096adb3435f18f445c6e500d73bc4
Gurneet kaur <68874066+Gurneet10@users.noreply.github.com>
2023-08-02 10:35:59 +0530

    Update Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,28 +1,53 @@
 pipeline {
-    agent any
-    
-    stages {
-        stage('Checkout') {
-            steps {
-                // Check out the repository from GitHub
-                git 'https://github.com/GurneetKaur10/spotifyclone.git'
-            }
-        }
-        
-        stage('Build') {
-            steps {
-                // Execute your build commands here
-                sh 'mvn clean install' // Or any build command specific to your project
-            }
-        }
-        
-        stage('Test') {
-            steps {
-                // Execute your test commands here
-                sh 'mvn test' // Or any test command specific to your project
-            }
-        }
-        
-        // Add more stages as needed (e.g., deploy, publish, etc.)
+agent any 
+
+   stages {
+
+    stage('Cloning Git') {
+
+      steps
+        {
+        /* Let's make sure we have the repository cloned to our workspace */
+       checkout scm
+        }  
     }
+    stage('SAST'){
+      steps{
+        sh 'echo SAST stage'
+       }
+    }
+
+    
+    stage('Build-and-Tag') {
+    /* This builds the actual image; synonymous to
+         * docker build on the command line */
+      steps{    
+        sh 'echo Build and Tag'
+          }
+    }
+
+    stage('Post-to-dockerhub') {
+     steps {
+        sh 'echo post to dockerhub repo'
+     }
+    }
+
+    stage('SECURITY-IMAGE-SCANNER'){
+      steps {
+        sh 'echo scan image for security'
+     }
+    }
+
+    stage('Pull-image-server') {
+      steps {
+         sh 'echo pulling image ...'
+       }
+      }
+    
+    stage('DAST') {
+      steps  {
+         sh 'echo dast scan for security'
+        }
+    }
+ }
 }
